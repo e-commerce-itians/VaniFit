79df89db590490b1b6c0e941a1043b69cc947b5b
hazemphoenix <hazem.mando408@gmail.com>
2025-05-15 15:38:18 +0300

    add profile page

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const routes = {
   "/": Home,
   "/login": Login,
   "/register": Register,
   "/about": About,
+  "/profile": Profile,
 };
 
 export default function router() {
