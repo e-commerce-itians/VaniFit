dab8027a2bb2d647f651959b92777985b9d97824
hazemphoenix <hazem.mando408@gmail.com>
2025-05-15 15:32:28 +0300

    add login and registeration template

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const routes = {
   "/": Home,
+  "/login": Login,
+  "/register": Register,
   "/about": About,
 };
 
