a68064d2d5b113bb6ca62fa3624416f49f6503e1
Adel Srour <adelmalys@hotmail.com>
2025-05-15 16:27:10 +0300

    Move style.css to overwrite

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./style.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./style.css";
 import router from "./router.js";
 
 window.addEventListener("DOMContentLoaded", router);
