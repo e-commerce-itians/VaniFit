b566881744e1a7693ac84e35f078432c4595f451
hazemphoenix <hazem.mando408@gmail.com>
2025-05-16 17:37:02 +0300

    add spinner component

---
+++ src/pages/Spinner.js
@@ -0,0 +1,10 @@
+export default function Spinner() {
+  return `
+  <div class="d-flex justify-content-center align-items-center vh-100">
+  <div class="spinner-border text-primary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
+
+    `;
+}
