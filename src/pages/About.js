b15459db2cfd92073b04272ae1983d6819a626f7
Adel Srour <adelmalys@hotmail.com>
2025-05-16 19:18:04 +0300

    Add observer to load js code on component init

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,13 @@
+import { observer } from "../observer";
+const componentID = "About";
+
 export default function About() {
+  observer(componentID, compLoaded);
   return `
       About Page <br>
       <a href="/" data-link>Go to home</a> 
     `;
 }
+
+//Javascript code to be executed once the home component is loaded
+const compLoaded = () => {};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import Getdata from "../utils/Getdata";
+import { observer } from "../observer";
+const componentID = "home";
 
 export default async function Home() {
+  observer(componentID, compLoaded);
   return `
-      Home Page <br>
-      <a href="/about" data-link>Go to about</a> 
-    `;
+      <div component="${componentID}">
+        Home Page <br>
+        <a href="/about" data-link>Go to about</a> 
+        <div id="counter">0</div>
+        <button class="btn btn-success" id="counterBtn">Counter</button>
+      </div>
+  `;
 }
+
+//Javascript code to be executed once the home component is loaded
+const compLoaded = () => {
+  const counterText = document.querySelector("#counter");
+  const counterBtn = document.querySelector("#counterBtn");
+  counterBtn.addEventListener("click", () => {
+    counterText.innerText = Number(counterText.innerText) + 1;
+  });
+};
