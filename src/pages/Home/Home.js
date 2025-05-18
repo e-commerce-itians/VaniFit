4aec4335ddcd4c3639c9caed36544a4a0fed0c1d
Abdelrahman Elkhouly <khoulyyy@gmail.com>
2025-05-18 23:41:32 +0300

    Fix: home page responsive to mobile layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,7 +15,7 @@ export default async function Home() {
       <p class="text-muted">
         Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.
       </p>
-      <a href="#" class="btn btn-dark btn-lg mt-3">Shop Now</a>
+      <a href="#" class="btn btn-dark btn-lg mt-3 rounded-pill">Shop Now</a>
 
       <!-- Stats -->
       <div class="row mt-4 text-center">
@@ -161,18 +161,14 @@ export default async function Home() {
   </div>
     <div class="container rounded-5" id="style-container">
     <h2 class="section-title py-3">BROWSE BY dress STYLE</h2>
-    <div class="d-sm-block d-md-block d-lg-flex">
-    <a href="#" class=""><img class="ms-5 " src="images/casual.png" class="rounded float-start" alt="..."></a>
-    <a href="#" > <img class="ms-5" src="images/formal.png" class="rounded float-start" alt="..."></a>
+    <div class="d-flex flex-wrap justify-content-center align-items-center gap-4">
+    <a href="#"><img class="img-fluid" src="images/casual.png" alt="Casual Style"></a>
+    <a href="#"><img class="img-fluid" src="images/formal.png" alt="Formal Style"></a>
+    <a href="#"><img class="img-fluid" src="images/party.png" alt="Party Style"></a>
+    <a href="#"><img class="img-fluid" src="images/gym.png" alt="Gym Style"></a>
     </div>
-    <div class="d-sm-block d-md-block d-lg-flex">
-    <a href="#"> <img class="ms-5 py-4" src="images/party.png" class="rounded float-start" alt="..."></a>
-    <a href="#"> <img class="ms-5 py-4" src="images/gym.png" class="rounded float-start" alt="..."></a>
-    </div>
-
     </div>
   `;
 }
 
-//Javascript code to be executed once the home component is loaded
 const compLoaded = () => { };
