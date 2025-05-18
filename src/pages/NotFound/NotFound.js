7604b92aad07fcac257b111306eca0f77061c149
mina-medhat-00 <mina.medhat.3141@gmail.com>
2025-05-18 03:13:40 +0300

    update Not Found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,10 +4,19 @@ const componentID = "notfound";
 
 export default function NotFound() {
   observer(componentID, compLoaded);
-  return `
-  <h1>404 not found</h1>
+  return /*html*/ `
+    <div
+      class="container d-flex flex-column justify-content-center align-items-center error-container text-center p-5"
+    >
+      <div class="error-code"><h1>404 Not Found</h1></div>
+      <div class="error-message mb-4">
+        Oops! The page you're looking for doesn't exist.
+      </div>
+      <button class="btn btn-dark">
+        <a href="/" data-link>Go Back Home</a>
+      </button>
+    </div>
   `;
 }
 
-//Javascript code to be executed once the home component is loaded
 const compLoaded = () => {};
