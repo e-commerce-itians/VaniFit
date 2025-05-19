ea9dc73153d1ffee711d57c6e4d26d940f61d37b
Adel Srour <adelmalys@hotmail.com>
2025-05-19 11:31:58 +0300

    Change Footer Styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,18 @@ const componentID = "footer";
 
 export default function Footer() {
   observer(componentID, compLoaded);
-  return /*html*/ `<footer component="${componentID}">
-        <ul class="footer-links">
-            <li><a href="#">Conditions of Use</a></li>
-            <li><a href="#">Privacy Notice</a></li>
-            <li><a href="#">Interest-Based Ads</a></li>
-        </ul>
-        <p class="copyright">&copy; 1996-2025, Amazon.com, Inc, or its affiliates</p>
-    </footer>`;
+  return /*html*/ `<div component="${componentID}">
+    <footer>
+      <ul class="footer-links">
+        <li><a href="#">Conditions of Use</a></li>
+        <li><a href="#">Privacy Notice</a></li>
+        <li><a href="#">Interest-Based Ads</a></li>
+      </ul>
+      <p class="copyright">
+        &copy; 1996-2025, Amazon.com, Inc, or its affiliates
+      </p>
+    </footer>
+  </div>`;
 }
 
 //Javascript code to be executed once the home component is loaded
